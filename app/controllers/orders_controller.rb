a52b04ae35b6dbccbf5c5fd02ec8f3082a424d34
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-05-29 21:24:03 +0300

    add product and order controllers, views, routes, request and routing tests

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,58 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = collection
+  end
+
+  def show
+    @order = resourse
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def edit
+    @order = resourse
+  end
+
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      redirect_to @order, notice: "Order was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @order = resourse
+
+    if @order.update(order_params)
+      redirect_to @order, notice: "Order was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @order = resourse
+
+    @order.destroy
+    redirect_to orders_url, notice: "Order was successfully destroyed."
+  end
+
+  private
+
+  def collection
+    Order.all
+  end
+
+  def resourse
+    Order.find(params[:id])
+  end
+
+  def order_params
+    params.fetch(:order, {})
+  end
+end
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,60 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
+
+  def index
+    @products = collection
+  end
+
+  def show
+    @product = resourse
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = resourse
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product, notice: "Product was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @product = resourse
+
+    if @product.update(product_params)
+      redirect_to @product, notice: "Product was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = resourse
+
+    @product.destroy
+    redirect_to products_url, notice: "Product was successfully destroyed."
+  end
+
+  private
+
+  def collection
+    Product.all
+  end
+
+  def resourse
+    Product.find(params[:id])
+  end
+
+  def product_params
+    params.fetch(:product, {})
+  end
+end
