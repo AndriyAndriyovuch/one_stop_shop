c9f16cf7562e382fb64c37a130642ac63c593384
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-05-29 23:11:33 +0300

    add request and routing tests

--- spec/routing/products_routing_spec.rb
+++ spec/routing/products_routing_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe ProductsController, type: :routing do
+  let!(:product) { create(:product, id: 13) }
+
   describe "routing" do
     it "routes to #index" do
       expect(get: "/products").to route_to("products#index")
@@ -11,11 +13,11 @@ RSpec.describe ProductsController, type: :routing do
     end
 
     it "routes to #show" do
-      expect(get: "/products/1").to route_to("products#show", id: "1")
+      expect(get: "/products/13").to route_to("products#show", id: "13")
     end
 
     it "routes to #edit" do
-      expect(get: "/products/1/edit").to route_to("products#edit", id: "1")
+      expect(get: "/products/13/edit").to route_to("products#edit", id: "13")
     end
 
 
@@ -24,15 +26,15 @@ RSpec.describe ProductsController, type: :routing do
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/products/1").to route_to("products#update", id: "1")
+      expect(put: "/products/13").to route_to("products#update", id: "13")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/products/1").to route_to("products#update", id: "1")
+      expect(patch: "/products/13").to route_to("products#update", id: "13")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/products/1").to route_to("products#destroy", id: "1")
+      expect(delete: "/products/13").to route_to("products#destroy", id: "13")
     end
   end
 end
