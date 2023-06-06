a5acfbffddeb22d1dc7c699aff21e28c6c0e211d
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-06-06 22:56:37 +0300

    change index to cart in orders, add dots to truncate

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post "buy", to: "products#buy"
   delete "cancel_shipping/:id", to: "products#cancel_shipping", as:'cancel_shipping'
 
+  get "cart", to: "orders#cart"
+
   resources :orders
   resources :products
 end
