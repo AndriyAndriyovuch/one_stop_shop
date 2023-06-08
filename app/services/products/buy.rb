07b9d3fe05d034562d80d65f1e4c50f8d93dc585
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-06-08 11:33:44 +0300

    use layer components in views, replace old font-awesome gem with CDN, add check if amount negative or zero when user buy product

--- app/services/products/buy.rb
+++ app/services/products/buy.rb
@@ -7,7 +7,7 @@ class Products::Buy
     product = Product.find(@params[:product_id])
 
     @params[:amount] = product.balance if product.balance < @params[:amount].to_i
-    @params[:amount] = 1 if @params[:amount].blank?
+    @params[:amount] = 1 if @params[:amount].blank? || @params[:amount].to_i <= 0
 
     { @params[:product_id] => @params[:amount].to_i }
   end
