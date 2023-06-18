715d34400746d50af78693fa08e56d1cf0347040
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-06-18 14:00:27 +0300

    fix new session design, mode full_name helper to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def full_name(model)
+    "#{model.first_name} #{model.last_name}"
+  end
 end
--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,8 +1,4 @@
 module OrderHelper
-  def full_name(order)
-    "#{order.first_name} #{order.last_name}"
-  end
-
   def session_product_sum(product)
     session.dig(:products, product.id.to_s) * product.price
   end
