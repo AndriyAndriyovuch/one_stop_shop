e0c0c40b072d53aad3d4c95fbf9c6d8871db2efa
AndriyAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-06-20 14:56:14 +0300

    remove useless crud methods and buttons

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,41 +7,6 @@ class ProductsController < ApplicationController
     @product = resourse
   end
 
-  def new
-    @product = Product.new
-  end
-
-  def edit
-    @product = resourse
-  end
-
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      redirect_to @product, notice: "Product was successfully created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    @product = resourse
-
-    if @product.update(product_params)
-      redirect_to @product, notice: "Product was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @product = resourse
-
-    @product.destroy
-    redirect_to products_url, notice: "Product was successfully destroyed."
-  end
-
   private
 
   def collection
@@ -50,9 +15,5 @@ class ProductsController < ApplicationController
 
   def resourse
     collection.find(params[:id])
-  end
-
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :balance)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "products#index"
 
+  get "products", to: "products#index"
+  get "products/:id", to: "products#show", as: "product"
+
   get "cart", to: "cart#show", as: 'cart'
   post "products/:id/buy", to: "cart#update", as: 'buy'
   post "products/:id/change_amount", to: "cart#update", as: 'change_amount'
@@ -8,5 +11,4 @@ Rails.application.routes.draw do
   delete "clean_cart", to: "cart#delete", as: "clean_cart"
 
   resources :orders
-  resources :products
 end
