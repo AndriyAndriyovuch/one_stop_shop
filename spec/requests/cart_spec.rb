d6860715da5e16b7f2310df2196ed4b658f97af9
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-06-22 22:39:41 +0300

    add request tests for cart

--- spec/requests/cart_spec.rb
+++ spec/requests/cart_spec.rb
@@ -1,70 +1,71 @@
-# # spec/requests/cart_spec.rb
-# require 'rails_helper'
+# spec/requests/cart_spec.rb
+require 'rails_helper'
 
-# RSpec.describe "Cart", type: :request do
-#   include RSpec::Rails::RequestExampleGroup
+RSpec.describe "Cart", type: :request do
 
+  let!(:product) { create(:product) }
 
-#   describe "GET /cart" do
-#     it "renders the cart page" do
-#       get cart_path
-#       expect(response).to have_http_status(200)
-#     end
-#   end
+  describe "GET /cart" do
+    it "renders the cart page" do
+      get cart_path
 
-#   describe "POST /products/:id/buy" do
-#     let(:product) { create(:product) }
+      expect(response).to be_successful
+    end
+  end
 
-#     it "adds the product to the cart" do
-#       post buy_product_path(product)
+  describe "POST /products/:id/buy" do
+    it "adds the product to the cart" do
+      post buy_product_path(product), params: {update_action: 'buy'}
 
-#       expect(session[:products][product.id.to_s]).to be_present
-#     end
+      expect(session[:products][product.id.to_s]).to be_present
+    end
 
-#     it "redirects back to the previous page" do
-#       post buy_product_path(product)
-#       expect(response).to redirect_to(request.referrer || root_path)
-#     end
-#   end
+    it "redirects back to the previous page" do
+      post buy_product_path(product)
 
-#   describe "POST /products/:id/change_amount" do
-#     let(:product) { create(:product) }
-#     let(:amount) { 5 }
+      expect(response).to redirect_to(request.referrer || root_path)
+    end
+  end
 
-#     it "updates the product amount in the cart" do
-#       post change_amount_product_path(product), params: { amount: amount }
-#       expect(session[:products][product.id.to_s]).to eq(amount)
-#     end
+  describe "POST /products/:id/change_amount" do
+    let(:amount) { 5 }
 
-#     it "redirects back to the previous page" do
-#       post change_amount_product_path(product), params: { amount: amount }
-#       expect(response).to redirect_to(request.referrer || root_path)
-#     end
-#   end
+    it "updates the product amount in the cart" do
+      post change_amount_product_path(product), params: { update_action: 'change', amount: amount }
+      expect(session[:products][product.id.to_s]).to eq(amount)
+    end
 
-#   describe "POST /products/:id/cancel_shipping" do
-#     let(:product) { create(:product) }
+    it "redirects back to the previous page" do
+      post change_amount_product_path(product), params: { amount: amount }
+      expect(response).to redirect_to(request.referrer || root_path)
+    end
+  end
 
-#     it "removes the product from the cart" do
-#       post cancel_shipping_product_path(product)
-#       expect(session[:products][product.id.to_s]).to be_nil
-#     end
+  describe "POST /products/:id/cancel_shipping" do
+    it "removes the product from the cart" do
+      post cancel_shipping_product_path(product), params: {update_action: 'delete'}
 
-#     it "redirects back to the previous page" do
-#       post cancel_shipping_product_path(product)
-#       expect(response).to redirect_to(request.referrer || root_path)
-#     end
-#   end
+      expect(session[:products]).to be_nil
+    end
 
-#   describe "DELETE /clean_cart" do
-#     it "clears the cart" do
-#       delete clean_cart_path
-#       expect(session[:products]).to be_empty
-#     end
+    it "redirects back to the previous page" do
+      post cancel_shipping_product_path(product)
 
-#     it "redirects to the products page" do
-#       delete clean_cart_path
-#       expect(response).to redirect_to(products_path)
-#     end
-#   end
-# end
+      expect(response).to redirect_to(request.referrer || root_path)
+    end
+  end
+
+  describe "DELETE /clean_cart" do
+    it "clears the cart" do
+      delete clean_cart_path
+
+      expect(session[:products]).to be_nil
+    end
+
+    it "redirects to the products page" do
+      delete clean_cart_path
+
+      expect(response).to redirect_to(products_path)
+    end
+  end
+end
