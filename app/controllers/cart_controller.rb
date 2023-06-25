f88a3b3d93d3f6b6ad4d1aa509454b1cace78acd
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2023-06-25 10:47:12 +0300

    move validation to before_action

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,19 +1,15 @@
 class CartController < ApplicationController
-  before_action :initialize_cart, only: %i[update]
+  before_action :initialize_cart, :cart_params_valid?, only: %i[update]
 
   def show
     @cart = Cart::Storage.new(session, params)
   end
 
   def update
-    if cart_params_valid?
-      service = Cart::ManagerService.new(session, cart_params)
-      service.call
+    service = Cart::ManagerService.new(session, cart_params)
+    service.call
 
-      redirect_back fallback_location: root_path, notice: (I18n.t "cart.notice.#{params[:action_type]}")
-    else
-      redirect_back fallback_location: root_path, alert: "Something went wrong"
-    end
+    redirect_back fallback_location: root_path, notice: (I18n.t "cart.notice.#{params[:action_type]}")
   end
 
   def delete
@@ -33,9 +29,11 @@ class CartController < ApplicationController
   end
 
   def cart_params_valid?
-    return false unless cart_params[:id].present? && cart_params[:id].to_i.is_a?(Integer)
-    return false unless cart_params[:action_type].present? && %w[add_product update_amount remove_product].include?(cart_params[:action_type])
-
-    true
+    unless cart_params[:id].present? &&
+           cart_params[:id].to_i.is_a?(Integer) &&
+           cart_params[:action_type].present? &&
+           %w[add_product update_amount remove_product].include?(cart_params[:action_type])
+      redirect_back fallback_location: root_path, alert: "Something went wrong" and return
+    end
   end
 end
