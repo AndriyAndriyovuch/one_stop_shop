20f0a826f6d4fd93d556abd9d8221a4438a167e4
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-07-09 23:52:18 +0300

    add mailer to devise

--- db/migrate/20230618084316_devise_create_users.rb
+++ db/migrate/20230618084316_devise_create_users.rb
@@ -16,6 +16,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
+
       ## Rememberable
       t.datetime :remember_created_at
 
